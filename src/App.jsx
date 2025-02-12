1ebdbbc2d3e48360ee22677a5002ab354e7526a8
mainaanthony <mainaanthonymwai@gmail.com>
2025-02-12 21:24:22 +0300

    readjust issue on route to use browserRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const MainLayout = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basseName="/pf">
       <Routes>
         {/* CV page (no header/footer) */}
         <Route path="/pf/cv" element={<CVPage />} />
